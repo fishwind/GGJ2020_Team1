d68fe416f6b5419826de42a9af51c3888aba8aec
adrianhayyj <49949920+adrianhayyj@users.noreply.github.com>
2020-02-01 17:02:35 +0800

    basic progress bar functionality

---
+++ Assets/Scripts/UI/ProgressBarController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarController : MonoBehaviour
+{
+    public Transform progressBar;
+    private Vector3 progressStartPos;
+    private Vector3 progressBarPos;
+    public Transform eventAnchor;
+
+    private float currTime;
+    private float gameTime;
+
+    private void Awake()
+    {
+        progressStartPos = progressBar.localPosition;
+        UpdateTimer(5);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            progressBar.localPosition = progressStartPos;
+            UpdateTimer(Random.Range(5, 10));
+        }
+
+        if (currTime < gameTime)
+            currTime += Time.deltaTime;
+        else currTime = gameTime;
+
+        if (gameTime < float.Epsilon) return;
+        progressBar.localPosition = Vector3.Lerp(progressBarPos, eventAnchor.localPosition, currTime / gameTime);
+    }
+
+    private void UpdateTimer(float timer)
+    {
+        gameTime = timer;
+        currTime = 0;
+
+        progressBarPos = progressBar.localPosition;
+        progressBarPos.y = 0;
+    }
+
+}
