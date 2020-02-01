f171805cabb74eef009f874463b066f5ad3f2fff
adrianhayyj <49949920+adrianhayyj@users.noreply.github.com>
2020-02-01 15:47:41 +0800

    basic menu functionality

---
+++ Assets/Scripts/UI/OptionsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsController : MonoBehaviour
+{
+    private void Update()
+    {
+        if (MainMenuController.Instance.IsOptionsDisplayed && Input.GetButton("Cancel"))
+        {
+            MainMenuController.Instance.Options();
+        }
+    }
+}
