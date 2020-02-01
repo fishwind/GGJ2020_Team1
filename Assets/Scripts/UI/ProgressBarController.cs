d1535d605459cd937e4ff5b78cdf0b386b669393
adrianhayyj <49949920+adrianhayyj@users.noreply.github.com>
2020-02-01 18:08:47 +0800

    progress bar update

--- Assets/Scripts/UI/ProgressBarController.cs
+++ Assets/Scripts/UI/ProgressBarController.cs
@@ -2,45 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class ProgressBarController : MonoBehaviour
 {
-    public Transform progressBar;
-    private Vector3 progressStartPos;
+    public Transform heroProgress;
+    private Image heroImage;
+    private Animator heroAnim;
     private Vector3 progressBarPos;
-    public Transform eventAnchor;
 
-    private float currTime;
+    public Transform startAnchor;
+    public Transform endAnchor;
+    public List<Transform> eventAnchors;
+
     private float gameTime;
+    private int rotation = 1;
 
     private void Awake()
     {
-        progressStartPos = progressBar.localPosition;
-        UpdateTimer(5);
+        heroImage = heroProgress.GetComponentInChildren<Image>();
+        progressBarPos = startAnchor.localPosition;
+
+        GlobalEvents.OnRepairGameplayStart += UpdateTimer;
+        GlobalEvents.OnRepairReturnDuration += UpdateTimer;
+
+        gameTime = 10;
+        StartProgressBar(endAnchor.localPosition);
     }
 
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.J))
         {
-            progressBar.localPosition = progressStartPos;
+            heroProgress.localPosition = startAnchor.localPosition;
             UpdateTimer(Random.Range(5, 10));
         }
+    }
 
-        if (currTime < gameTime)
-            currTime += Time.deltaTime;
-        else currTime = gameTime;
+    private void ResetProgressBar()
+    {
+        Count = 0;
+    }
 
-        if (gameTime < float.Epsilon) return;
-        progressBar.localPosition = Vector3.Lerp(progressBarPos, eventAnchor.localPosition, currTime / gameTime);
+    private void StartProgressBar(Vector3 destination)
+    {
+        Count++;
+        heroProgress.DOLocalMove(destination, gameTime).OnComplete(ReachedEnd);
+    }
+
+    private int Count = 0;
+    private void ReachedEnd()
+    {
+        GlobalEvents.SendHeroQuestComplete();
+
+        rotation *= -1;
+        heroImage.transform.DOScaleX(rotation, 0.3f);
+
+        if (Count > 0 && Count < 2)
+            StartProgressBar(startAnchor.localPosition);
     }
 
     private void UpdateTimer(float timer)
     {
         gameTime = timer;
-        currTime = 0;
 
-        progressBarPos = progressBar.localPosition;
+        progressBarPos = heroProgress.localPosition;
         progressBarPos.y = 0;
     }
 
