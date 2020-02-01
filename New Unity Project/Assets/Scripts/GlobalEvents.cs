690d59fead2372ef8f86c2de5cf38db0595fa375
benjamin.chua <chuaywb@gmail.com>
2020-02-01 10:33:26 +0800

    Added new GlobalEvents class
    Added new GlobalEvents class - Ben

---
+++ New Unity Project/Assets/Scripts/GlobalEvents.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void GenericEvent();
+public delegate void GenericIntEvent(int val);
+
+//this class has all the communication events. All features should use this to talk to each other and not directly access.
+
+public static class GlobalEvents
+{
+    public static GenericEvent OnPlayerApproachHouse;
+    public static void SendPlayerApproachHouse()
+    {
+        OnPlayerApproachHouse?.Invoke();
+    }
+    public static GenericEvent OnPlayerStartDestroyDoor;
+    public static void SendPlayerStartDestroyDoor()
+    {
+        OnPlayerStartDestroyDoor?.Invoke();
+    }
+    public static GenericEvent OnPlayerDestroyedDoor;
+    public static void SendPlayerDestroyedDoor()
+    {
+        OnPlayerDestroyedDoor?.Invoke();
+    }
+    public static GenericIntEvent OnPlayerStartDestroyAll;
+    public static void SendPlayerStartDestroyAll()
+    {
+        OnPlayerStartDestroyAll?.Invoke();
+    }
+    public static GenericIntEvent OnPlayerDestroyedAll;
+    public static void SendPlayerDestroyedAll()
+    {
+        OnPlayerDestroyedAll?.Invoke();
+    }
+    public static GenericEvent OnPlayerStartLeave;
+    public static void SendPlayerStartLeave()
+    {
+        OnPlayerStartLeave?.Invoke();
+    }
+    public static GenericIntEvent OnPlayerLeaveComplete;
+    public static void SendPlayerLeaveComplete()
+    {
+        OnPlayerLeaveComplete?.Invoke();
+    }
+}
