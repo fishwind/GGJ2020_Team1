38ef937db8f1f92d9baf976d2d18d66ee9acd0fa
adrianhayyj <49949920+adrianhayyj@users.noreply.github.com>
2020-02-02 11:01:17 +0800

    fix cat

--- Assets/Scripts/Items/ItemScript/Cat.cs
+++ Assets/Scripts/Items/ItemScript/Cat.cs
@@ -2,45 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cat : Entity, IBreakable
+public class Cat : Entity
 {
-    #region Init / Destroy
-    private void Awake()
-    {
-        GlobalEvents.OnPlayerStartDestroyAll += AttemptBreak;
-    }
 
-    private void OnDestroy()
-    {
-        GlobalEvents.OnPlayerStartDestroyAll -= AttemptBreak;
-    }
-    #endregion
-
-    void Break()
-    {
-        currItemState = ItemStates.Broken;
-        currItemActionState = ItemActionState.RepairSweep;
-
-        UpdateItemMesh();
-        // TODO: Animations, Play Sounds
-        StartVisualFeedback();
-    }
-
-    #region IBreakable
-    public void AttemptBreak(int itemTier)
-    {
-        // TODO: Check if Hero Lvl Strng Enuff to break
-        if (!CANIBREAK(itemTier)) return;
-
-        // Only Break if Already Fixed
-        if (currItemState == ItemStates.Fixed)
-        {
-            Break();
-        }
-        else
-        {
-            // TODO: Feedback to player/system that pot not fixed
-        }
-    }
-    #endregion
 }
