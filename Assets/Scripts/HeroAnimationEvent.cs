00d71d69353687430c546c2031043456b6deb3d9
fishwind <e0003228@u.nus.edu>
2020-02-02 12:24:17 +0800

    Add hero footstep

---
+++ Assets/Scripts/HeroAnimationEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroAnimationEvent : MonoBehaviour
+{
+    private AudioSource m_Asource = null;
+    public AudioClip m_FootstepClip1 = null;
+    public AudioClip m_FootstepClip2 = null;
+
+    private void Start() {
+        m_Asource = GetComponent<AudioSource>();
+        if(!m_Asource)
+            m_Asource = this.gameObject.AddComponent<AudioSource>();
+    }
+
+    private void PlayFootstep1() {
+        m_Asource.PlayOneShot(m_FootstepClip1);
+    }
+
+    private void PlayFootstep2() {
+        m_Asource.PlayOneShot(m_FootstepClip2);
+    }
+}
